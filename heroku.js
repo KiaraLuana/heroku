2766c11c0788cfe90794c29a7b960eb7296ec894
Kiara <Kiara@kiaras-air.hfu-open.fu.hs-furtwangen.de>
2017-06-20 15:32:37 +0200

    Style

--- heroku.js
+++ heroku.js
@@ -25,33 +25,33 @@ function handleRequest(_request, _response) {
     let query = Url.parse(_request.url, true).query;
     let selection = "";
     if (query["Vanilla "] != "0")
-        selection += query["Vanilla "] + " scoop(s) of Vanilla, <br>";
+        selection += query["Vanilla "] + " scoop(s) of Vanilla <br>";
     if (query["Chocolate "] != "0")
-        selection += query["Chocolate "] + " scoop(s) of Chocolate, <br>";
+        selection += query["Chocolate "] + " scoop(s) of Chocolate <br>";
     if (query["Stracciattela "] != "0")
-        selection += query["Stracciatella "] + " scoop(s) of Stracciattela, <br>";
+        selection += query["Stracciatella "] + " scoop(s) of Stracciattela <br>";
     if (query["Pistachio "] != "0")
-        selection += query["Pistachio "] + " scoop(s) of Pistachio, <br>";
+        selection += query["Pistachio "] + " scoop(s) of Pistachio <br>";
     if (query["Cinnamon "] != "0")
-        selection += query["Cinnamon "] + " scoop(s) of Cinnamon, <br>";
+        selection += query["Cinnamon "] + " scoop(s) of Cinnamon <br>";
     if (query["Tiramisu "] != "0")
-        selection += query["Tiramisu "] + " scoop(s) of Tiramisu, <br>";
+        selection += query["Tiramisu "] + " scoop(s) of Tiramisu <br>";
     if (query["Coconut "] != "0")
-        selection += query["Coconut "] + " scoop(s) of Coconut, <br>";
+        selection += query["Coconut "] + " scoop(s) of Coconut <br>";
     if (query["Cookies "] != "0")
-        selection += query["Cookies "] + " scoop(s) of Cookies, <br>";
+        selection += query["Cookies "] + " scoop(s) of Cookies <br>";
     if (query["Coffee "] != "0")
-        selection += query["Coffee "] + " scoop(s) of Coffee, <br>";
+        selection += query["Coffee "] + " scoop(s) of Coffee <br>";
     if (query["Caramel "] != "0")
-        selection += query["Caramel "] + " scoop(s) of Caramel, <br>";
+        selection += query["Caramel "] + " scoop(s) of Caramel <br>";
     if (query["Bueno "] != "0")
-        selection += query["Bueno "] + " scoop(s) of Bueno, <br>";
+        selection += query["Bueno "] + " scoop(s) of Bueno <br>";
     if (query["Rafaello "] != "0")
-        selection += query["Rafaello "] + " scoop(s) of Rafaello, <br>";
+        selection += query["Rafaello "] + " scoop(s) of Rafaello <br>";
     if (query["Almond "] != "0")
-        selection += query["Almond "] + " scoop(s) of Almond, <br>";
+        selection += query["Almond "] + " scoop(s) of Almond <br>";
     if (query["Hazelnut "] != "0")
-        selection += query["Hazelnut "] + " scoop(s) of Hazelnut, <br>";
+        selection += query["Hazelnut "] + " scoop(s) of Hazelnut <br>";
     _response.write("Hello " + query["Name"] + ", <br> Your order has been submitted! <br><br>");
     _response.write("Your order: <br>" + selection);
     _response.end("</body></html>");
