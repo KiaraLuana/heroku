ce9f4f17220b96be142cd67704029898e24ea7aa
Kiara <Kiara@kiaras-air.fritz.box>
2017-06-21 15:59:24 +0200

    node test

--- heroku-a10-4.js
+++ heroku-a10-4.js
@@ -15,56 +15,14 @@ function handleListen() {
 function handleRequest(_request, _response) {
     console.log("Request received");
     console.log(_request.url);
+    let query = Url.parse(_request.url, true).query;
+    console.log(query);
+    let key;
+    for (key in query)
+        console.log(key + ":" + query[key]);
     _response.setHeader("Access-Control-Allow-Origin", "*");
     _response.setHeader("content-type", "text/html; charset=utf-8");
-    //HTML DOCUMENT & STYLE
-    _response.write("<html>");
-    _response.write("<head> <link rel='stylesheet' type='text/css' href='style.css'> </head>");
-    _response.write("<body>");
-    //URL QUERY & OUTPUT
-    let query = Url.parse(_request.url, true).query;
-    let selection = "";
-    let top = "";
-    //Eissorten
-    if (query["Vanilla "] != "0")
-        selection += query["Vanilla "] + " scoop(s) of Vanilla <br>";
-    if (query["Chocolate "] != "0")
-        selection += query["Chocolate "] + " scoop(s) of Chocolate <br>";
-    if (query["Stracciatella "] != "0")
-        selection += query["Stracciatella "] + " scoop(s) of Stracciatella <br>";
-    if (query["Pistachio "] != "0")
-        selection += query["Pistachio "] + " scoop(s) of Pistachio <br>";
-    if (query["Cinnamon "] != "0")
-        selection += query["Cinnamon "] + " scoop(s) of Cinnamon <br>";
-    if (query["Tiramisu "] != "0")
-        selection += query["Tiramisu "] + " scoop(s) of Tiramisu <br>";
-    if (query["Coconut "] != "0")
-        selection += query["Coconut "] + " scoop(s) of Coconut <br>";
-    if (query["Cookies "] != "0")
-        selection += query["Cookies "] + " scoop(s) of Cookies <br>";
-    if (query["Coffee "] != "0")
-        selection += query["Coffee "] + " scoop(s) of Coffee <br>";
-    if (query["Caramel "] != "0")
-        selection += query["Caramel "] + " scoop(s) of Caramel <br>";
-    if (query["Bueno "] != "0")
-        selection += query["Bueno "] + " scoop(s) of Bueno <br>";
-    if (query["Rafaello "] != "0")
-        selection += query["Rafaello "] + " scoop(s) of Rafaello <br>";
-    if (query["Almond "] != "0")
-        selection += query["Almond "] + " scoop(s) of Almond <br>";
-    if (query["Hazelnut "] != "0")
-        selection += query["Hazelnut "] + " scoop(s) of Hazelnut <br>";
-    //Toppings
-    if (query["Whipped Cream "] != "off")
-        top += "with whipped cream<br>";
-    if (query["Brittle "] != "off")
-        top += "with brittle<br>";
-    if (query["Couverture "] != "off")
-        top += "with couverture<br>";
-    _response.write("Thank you, " + query["Name"] + "! <br> Your order has been submitted! <br><br>");
-    _response.write("<br>Your order: <br><br>" + selection + top);
-    _response.write("<br><br><br>Your delivery data: <br><br>Name: " + query["Name"] + "<br>Street: " + query["Street"]
-        + "<br>Location: " + query["Location"] + "<br>Delivery option: " + query["Options"]);
-    _response.end("</body></html>");
+    _response.write("Ich höre Stimmen!");
+    _response.end();
 }
 //# sourceMappingURL=heroku-a10-4.js.map
